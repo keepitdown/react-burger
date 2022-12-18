be9c83bb9313780872ce873fba215f4161e2ad31
Artem <mntp112@yandex.ru>
2022-12-19 01:06:23 +0300

    refactor: remove default markup and styles from index

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import logo from '../../images/logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import { Button } from '@ya.praktikum/react-developer-burger-ui-components';
 
@@ -8,24 +7,7 @@ function App() {
     .then((resp) => resp.json())
     .then((data) => console.log(data));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button htmlType="button" type="primary" size="large" extraClass="mt-2">Кнопка</Button>
-        <div className="mt-2 text text_type_main-large text_color_inactive">Текст</div>
-      </header>
-    </div>
+    <div></div>
   );
 }
 
