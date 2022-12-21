ea619deee3946cc1691f60b10c8e927da15b231c
Artem <mntp112@yandex.ru>
2022-12-21 03:09:34 +0300

    fix: rightside nav items margin

--- src/components/nav-bar/nav-bar.jsx
+++ src/components/nav-bar/nav-bar.jsx
@@ -10,12 +10,16 @@ function NavBar({ children, firstRightItem }) {
           children.map?.((item, index) => (
             <li
               key={index}
-              className={'mr-2' + (index === (Number(firstRightItem) - 1) ? (' ' + styles['list-item_first-right']) : '')}
+              className={
+                (index < (Number(firstRightItem) - 1) ? 'mr-2' : '')
+                + (index > (Number(firstRightItem) - 1) ? 'ml-2' : '')
+                + (index === (Number(firstRightItem) - 1) ? (' ' + styles['list-item_first-right']) : '')
+              }
             >
               {item}
             </li>
             ))
-          ?? (<li className="mr-2">{children}</li>)
+          ?? (<li>{children}</li>)
         }
       </ul>
     </nav>
