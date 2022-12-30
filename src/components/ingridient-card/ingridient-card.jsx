0793369120fcd3d8dfbb67a6949135e0c216eb5f
Artem <mntp112@yandex.ru>
2022-12-31 02:18:47 +0300

    feat: ingridient category and card styles and render logic

--- src/components/ingridient-card/ingridient-card.jsx
+++ src/components/ingridient-card/ingridient-card.jsx
@@ -2,11 +2,11 @@ import React, {useState, useEffect} from 'react';
 import styles from './ingridient-card.module.css';
 import { Counter, CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 
-function IngridientCard({ data, quantity }) {
+function IngridientCard({ data, quantity, isEven }) {
 
   return (
-    <li className={styles.container}>
-      {(quantity > 0) && <Counter count={quantity} size="default" extraClass="m-1" />}
+    <li className={styles.container + (isEven ? ' ml-6' : '')}>
+      {!!quantity && <Counter count={quantity} size="default" />}
       <img src={data.image} alt={data.name} className="ml-4 mr-4" />
       <div className={styles['price-tag'] + ' mt-1 mb-1'}>
         <p className="text text_type_digits-default mr-2">{data.price}</p>
--- src/components/ingridients-category/ingridients-category.jsx
+++ src/components/ingridients-category/ingridients-category.jsx
@@ -5,16 +5,20 @@ import IngridientCard from '../ingridient-card/ingridient-card';
 function IngridientsCategory({ children, categoryData }) {
 
   return (
-    <>
+    <article>
       <h2 className="text text_type_main-medium">{children}</h2>
-      <div className="pt-6 pb-10 pl-4">
-        <ul className={styles['ingridients-list'] + ' pl-2'}>
-          {categoryData &&
-            <IngridientCard data={categoryData[0]} quantity={1} />
+      <div className={styles.container + ' pt-6 pr-4 pb-10 pl-4'}>
+        <ul className={styles['ingridients-list']}>
+          {
+            categoryData && categoryData.map((itemData, index) => {
+              return (
+                <IngridientCard key={index} data={itemData} quantity={1} isEven={index % 2} />
+              )
+            })
           }
         </ul>
       </div>
-    </>
+    </article>
   )
 }
 
