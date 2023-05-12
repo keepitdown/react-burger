d2a7b0631e445b4887c81138113b9237e4eb291e
Artem <mntp112@yandex.ru>
2023-05-12 16:12:09 +0300

    feat: add gh-pages package

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import thunk from 'redux-thunk';
 import rootReducer from './services/reducers';
 import ReactDOM from 'react-dom/client';
@@ -26,9 +26,9 @@ const store = createStore(rootReducer, enhancer);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter>
+      <BrowserRouter>
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
